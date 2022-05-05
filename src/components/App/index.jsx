dfc9cab044c7576e321b3683b129437e04c17470
codesfromshad <contact@codesfromshad.com>
2022-05-06 02:50:46 +0600

    Fix state on App component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [],
-      playlistName: '',
+      playlistName: 'New Playlist',
       playlistTracks: []
     }
     
